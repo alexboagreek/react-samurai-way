66b05959704e8f19cb53f4ac0160c555612a0ab8
Alex <olivesargreek@gmail.com>
2021-06-15 12:24:28 +0300

    create: new-branch

---
+++ src/Technologies.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Tech = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+
+    );
+
+}
+
+export default Tech;
